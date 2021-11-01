fa9563c5699b94f881b0108964f2e8fb27ef0bca
Luca Bron <lucagarrybron@gmail.com>
2021-11-01 23:46:54 +0900

    made progress in many different files. Switched from iPython to normal python. Made the map work. Ready to code the fun part!

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-function myFunction() {
-	document.getElementById("demo").innerHTML = 5 + 6;
-}
+// initialize the map
+var map = L.map('map').setView([42.35, -71.08], 13);
+
+// load a tile layer
+L.tileLayer('http://tiles.mapc.org/basemap/{z}/{x}/{y}.png',
+{
+attribution: 'Tiles by <a href="http://mapc.org">MAPC</a>, Data by <a href="http://mass.gov/mgis">MassGIS</a>',
+	maxZoom: 17,
+	minZoom: 9
+}).addTo(map);
---
+++ main.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+# import matplotlib.pyplot as plt
+# import plotly.express as px
+# import seaborn as sns
+# import plotnine
+
+#%%
+df = pd.read_csv('data/owid-co2-data.csv')
+
+#%%
+# select important columns
+df = df[['iso_code',
+ 'country',
+ 'year',
+ 'co2',
+ 'co2_growth_prct',
+ 'co2_growth_abs',
+ 'co2_per_capita',
+ 'co2_per_gdp',
+ 'share_global_co2',
+ 'cumulative_co2',
+ 'share_global_cumulative_co2',
+ 'ghg_per_capita',
+ 'methane',
+ 'methane_per_capita',
+ 'nitrous_oxide',
+ 'nitrous_oxide_per_capita',
+ 'primary_energy_consumption',
+ 'energy_per_capita',
+ 'energy_per_gdp',
+ 'population',
+ 'gdp']]
+
+#%%
+print(df)
