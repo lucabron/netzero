d0d142d28a1c464098d5d07ac33d58b775fa2186
Luca Bron <lucagarrybron@gmail.com>
2021-12-02 18:24:30 +0900

    Country selection interactive with Javascript callbacks.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # Packages used are numpy, pandas and bokeh. They can be found on pip and conda.
+from os.path import dirname, join
+
 import numpy as np
 import pandas as pd
 from bokeh.io import show
@@ -12,7 +14,8 @@ from bokeh.layouts import column
 pd.set_option('max_columns', None)
 
 #%%
-df = pd.read_csv('data/owid-co2-data.csv')
+#pd.read_csv(join(dirname(__file__), 'data/2015_weather.csv'))
+df = pd.read_csv(join(dirname(__file__),'data','owid-co2-data.csv'))
 
 #%%
 print(df.columns)
@@ -54,6 +57,7 @@ df = df[['iso_code',
 # French Equatorial Africa
 # French West Africa
 # =============================================================================
+
 excluded = df[df['country'].isin(['Kuwaiti Oil Fires','Panama Canal Zone'])].index
 df.drop(excluded, inplace=True)
 
@@ -71,6 +75,7 @@ print(dfgraph.head())
 #%%
 print(dfgraph[pd.isnull(dfgraph['co2'])==True]) 
 dfgraphtemp = dfgraph[pd.isnull(dfgraph['co2'])==True]
+print('-------')
 print(dfgraphtemp['country'].unique())
 
 # =============================================================================
@@ -82,7 +87,7 @@ print(dfgraphtemp['country'].unique())
 # Micronesia #no data
 # Namibia #no data
 # =============================================================================
-
+print('-------')
 dfgraphtemp = dfgraph[dfgraph['year'].isin([2015,2019])]
 print(dfgraphtemp[dfgraphtemp['co2'].isna()])
 
@@ -95,7 +100,7 @@ dfgraph = dfgraph[-dfgraph['country'].isin(["Cote d'Ivoire","Palau"])]
 #%%
 #create the new rows for the forecast
 dfgraphcolumns_list = list(dfgraph)
-list_countries = dfgraphtemp['country'].unique()
+list_countries = list(dfgraphtemp['country'].unique())
 current_year = dfgraph['year'].max()
 nb_years = 2050-current_year+1 #number of years to forecast
 dfgraphfc = dfgraph.copy()
@@ -131,21 +136,30 @@ dfgraphfc = dfgraphfc.append(data, True)
 print(dfgraphfc)
 
 #%%
-#dfgraphfc = dfgraphfc[dfgraphfc['country']=='Switzerland']
+#Bokeh
 
-#%%
+#Default
+#dfgraphfc = dfgraphfc[dfgraphfc['country']=='World']
+
 #transform pandas df into ColumnDataSource 
+title = "World"
 source = ColumnDataSource(dfgraphfc)
+render = ColumnDataSource(
+    {
+        "year": dfgraphfc[dfgraphfc['country']=='World']["year"],
+        "co2": dfgraphfc[dfgraphfc['country']=='World']["co2"],
+    }
+)
 
 # create a new plot (with a title) using figure
-p = figure(plot_width=1200, plot_height=550, title="Switzerland CO2 emissions",
+p = figure(plot_width=1200, plot_height=550, title=title,
            x_range=Range1d(1980, 2050, bounds=(None,2050)),
            tools=[PanTool(dimensions='width'), SaveTool(), WheelZoomTool(), ResetTool()])
 
 # add a line renderer
-p.line("year", "co2" , line_width=2, source=source, legend_label='CO2 emissions')
-p.line([2015, 2050], [dfgraphfc.loc[dfgraphfc['year'] == 2015]['co2'],0],
-       line_width=2, color = 'red', legend_label='Path to netzero in 2050')
+p.line("year", "co2", source=render, line_width=2, legend_label='CO2 emissions', line_alpha=0.5)
+#p.line([2015, 2050], [dfgraphfc.loc[dfgraphfc['year'] == 2015]['co2'],0],
+#       line_width=2, color = 'red', legend_label='Path to netzero in 2050')
 COP21 = Span(location=2015, dimension='height', line_color='green',
              line_dash='dotted', line_width=2)
 p.add_layout(COP21)
@@ -155,12 +169,46 @@ p.xaxis.axis_label = 'Year'
 p.yaxis.axis_label = 'CO2 emissions [mio of tons]'
 p.add_layout(Label(x=2016, text='COP21', text_color='green'))
     
-#dropdown menu
-select = Select(title="Country:", value="World", options=list(df['country'].unique()))
-select.js_on_change("value", CustomJS(code="""
-    console.log('select: value=' + this.value, this.toString())
-"""))
+#dropdown     
+select = Select(title="Country:", value="World", options=list_countries, width = 300)
 
+#def update_plot(attr, old, new):
+#    p.title.text = dfgraphfc[dfgraphfc['country']==select.value]
+#    country_selected = select.value
+#    select.options = list(df[country_selected].values)
+
+callback_data = CustomJS( args = dict(render=render, source=source, select=select),
+code="""
+//Filter the countries
+var data=source.data;
+var country=data['country'];
+
+var a=data['year'];
+var b=data['co2'];
+
+var f=select.value;
+
+var x=[];
+var y=[];
+
+for(var i=0;i<a.length; i++){
+        if(country[i]==f){
+                         x.push(a[i]);
+                         y.push(b[i]);
+                    }
+        }
+
+render.data['year']=x;
+render.data['co2']=y;
+
+// Apply change
+render.change.emit();
+""")
+
+select.js_on_change("value", callback_data)
+select.js_link('value', p.title, 'text')
+    
 layout = column(select, p)
 
-show(layout) # show the results
+show(layout)
+#curdoc().add_root(layout) # show the results
